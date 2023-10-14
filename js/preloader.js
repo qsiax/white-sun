1397c07c409c8c0f09265eb2a542d58068533509
Ivanov Nikolai <102044962+qsiax@users.noreply.github.com>
2023-10-14 13:59:20 +0900

    commit

---
+++ js/preloader.js
@@ -0,0 +1,37 @@
+TweenMax.to('.firts', 1.5, {
+	delay: .7,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+TweenMax.to('.second', 1.5, {
+	delay: .9,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+TweenMax.to('.third', 1.5, {
+	delay: 1.1,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+
+TweenMax.to('.four', 1.5, {
+	delay: 1.3,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+TweenMax.to('.five', 1.5, {
+	delay: 1.5,
+	height: "0%",
+	ease: Expo.easeInOut
+})
+
+
+TweenMax.to('.six', 1.5, {
+	delay: 1.7,
+	height: "0%",
+	ease: Expo.easeInOut
+})
